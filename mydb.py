6904799415393e99c2214e70ca71e72b34d0d43e
Nagesh Acharya <nagesh422002@gmail.com>
2024-10-26 14:50:57 +0530

    DJANGO CRM first project

---
+++ mydb.py
@@ -0,0 +1,19 @@
+# Install mysql on your computer
+# pip install mysql
+# pip install mysql-connector
+# OR
+# pip install mysql-connector-python
+
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = '1234'
+)
+
+# prepare a cursor object
+curssorObject = dataBase.cursor()
+
+# Create a dataBase
+curssorObject.execute("CREATE DATABASE elderco")
